31604a0e062b2ce24ab1c5c1ce1c01987d030e87
Raffaele Valenti <raffaele-valenti@protonmail.com>
2024-05-23 16:57:14 +0200

    feat(main): add lazy load on main application component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react';
+import { StrictMode, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './app.tsx';
-import './index.css';
+import './index.sass';
+
+const App = lazy(() => import('./app.tsx'));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
